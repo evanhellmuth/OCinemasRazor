b1e3cb0f87f850f8f0b6a0294987b9dc9eb34078
Ben Houston-Edwards <benjamin.houston.edwards@gmail.com>
2018-01-07 00:05:55 -0800

    Added movies to database

--- razor.py
+++ razor.py
@@ -107,7 +107,7 @@ def add_movie():
 	poster_filename=''
 	if request.form['poster_url']:
 		poster_filename = '_'.join(request.form['title'].lower().split())
-		poster_filename = poster_filename + request.form['year'] + '.jpg'
+		poster_filename = poster_filename + '_' + request.form['year'] + '.jpg'
 		scraper.download_image(request.form['poster_url'], 'static/movie_posters/'+poster_filename)
 
 	db = get_db()
